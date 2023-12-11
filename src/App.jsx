30de5c8696555d55480b095fc6377eef6538b06c
Amir Pasha Zarghami <apashazarghami@gmail.com>
2023-12-11 10:04:13 +0330

    init project and home route

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Layout>
+    </>
+  )
+}
+
+export default App
